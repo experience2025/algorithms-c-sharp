b98591fe09a1a2ea018482953449e24df1d743e7
Kirill S. <93134634+CasualMe@users.noreply.github.com>
2025-03-05 22:44:02 +0500

    Update Program.cs

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,77 +4,137 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int[,] example = new int[4, 4]
+        int[,] matrix = new int[6, 6] 
         {
-            { 12, 5, 6, 1}, { 7, 8, 9, 4 }, { 11, -1, 2, 5 }, { 20, 3, 6, 9 }
+            { 1, 2, 2, 1, 3, 4 },
+            { 3, 1, 1, 5, 7, 6 },
+            { 3, 4, 1, 2, 7, 6 },
+            { 5, 7, 1, 6, 4, 4 },
+            { 5, 9, 2, 3, 5, 8 },
+            { 2, 2, 1, 3, 1, 6 },
         };
-
-        calculateDeterminant(example);
+        
+        Dictionary<int, List<string> > result = new Dictionary<int, List<string> >();
+        result = CalculatePath(matrix);
+        
+        string pathString = "";
+        
+        foreach (var pair in result)
+        {
+            foreach (var index in pair.Value)
+            {
+                pathString += index + " | ";
+            }
+        
+            Console.WriteLine($"Длина пути: {pair.Key}  Путь: {pathString}");
+        }
     }
 
-public static long calculateDeterminant(int[,] matrix)
+static Dictionary< int, List<string> > CalculatePath(int[,] matrix)
 {
-    if (matrix.GetLength(0) != matrix.GetLength(1))
-    {
-        throw new ArgumentException("Матрица не квадратная");
-    }
+    List<string> path = new List<string>();
+    int[,] coeffMatrix = (int[,])matrix.Clone();
+    Dictionary<int, List<string> > result = new Dictionary<int, List<string> >();
 
-    bool flag = false;
+    int cumulative = 0;
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(0); j++)
-        {
-            if (matrix[i, j] != 0 || matrix.GetUpperBound(0) + 1 == 1) flag = true;
-        }
+        coeffMatrix[i, 0] = matrix[i, 0] + cumulative;
+        path.Add(string.Format("{0}, 0 ", i));
+        cumulative = matrix[i, 0] + cumulative;
     }
 
-    if(flag == false)
+    if (matrix.GetLength(1) == 1)
     {
-        throw new ArgumentException("Матрица вырожденная");
+        int pathLength1 = coeffMatrix[coeffMatrix.GetLength(0) - 1, coeffMatrix.GetLength(1) - 1];
+        result.Add(pathLength1, path);
+        return result;
     }
-
-    if (matrix.GetUpperBound(0) + 1 == 1)
-    {
-        return (long)matrix[0, 0];
-    }
-
     else
     {
-
-        long cumulative = 0;
-
-        for(int i = 0; i < matrix.GetLength(1); i++)
-        {
-            int[,] minor = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
-
-            for(int j = 1; j < matrix.GetLength(0); j++)
-            {
-                int y = 0;
-
-                for (int k = 0; k < matrix.GetLength(1); k++)
-                {
-
-                    if (k == i)
-                    {
-                        continue;
-                    }
-
-                    minor[j - 1, y] = matrix[j, k];
-
-                    y++;
-                 
-                }
-
-            }
-
-            cumulative += (long)Math.Pow(-1, 1 + i + 1) * matrix[0, i] * calculateDeterminant(minor);
-
-        }
-
-        return cumulative;
+        path = new List<string>();
     }
 
-}
+    cumulative = 0;
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        coeffMatrix[0, i] = matrix[0, i] + cumulative;
+        path.Add(string.Format("0, {0} ", i));
+        cumulative = matrix[0, i] + cumulative;
+    }
+
+    if (matrix.GetLength(0) == 1)
+    {
+        int pathLength2 = coeffMatrix[coeffMatrix.GetLength(0) - 1, coeffMatrix.GetLength(1) - 1];
+        result.Add(pathLength2, path);
+        return result;
+    }
+    else
+    {
+        path = new List<string>();
+    }
+
+    for (int i = 1; i < coeffMatrix.GetLength(0); i++)
+    {
+        for (int j = 1; j < coeffMatrix.GetLength(1); j++)
+        {
+            bool comparisonResult = coeffMatrix[i - 1, j] < coeffMatrix[i, j - 1];
+
+            if (comparisonResult)
+            {
+                coeffMatrix[i, j] = coeffMatrix[i - 1, j] + coeffMatrix[i, j];
+            }
+            else
+            {
+                coeffMatrix[i, j] = coeffMatrix[i, j - 1] + coeffMatrix[i, j];
+            }
+        }
+    }
+
+    for (int i = 0; i < coeffMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < coeffMatrix.GetLength(1); j++)
+        {
+            Console.WriteLine(coeffMatrix[i, j]);
+        }
+    }
+
+    int rowPosition = coeffMatrix.GetLength(0) - 1;
+    int columnPosition = coeffMatrix.GetLength(1) - 1;
+
+    while(rowPosition>=0 && columnPosition>=0)
+    {
+        if(rowPosition == 0)
+        {
+            path.Add ($" {rowPosition}, {columnPosition}");
+            columnPosition--;
+            continue;
+        }
+
+        if (columnPosition == 0)
+        {
+            path.Add($" {rowPosition}, {columnPosition}");
+            rowPosition--;
+            continue;
+        }
+
+        if (coeffMatrix[rowPosition,columnPosition - 1] < coeffMatrix[rowPosition - 1, columnPosition])
+        {
+            path.Add($" {rowPosition}, {columnPosition}");
+            columnPosition--;
+        }
+        else
+        {
+            path.Add($" {rowPosition}, {columnPosition}");
+            rowPosition--;
+        }
+    }
+
+    int pathLength = coeffMatrix[coeffMatrix.GetLength(0) - 1, coeffMatrix.GetLength(1) - 1];
+    result.Add(pathLength, path);
+
+    return result;
 
 }
