bfc8478b7c4987070f7ae52b509c72a125f1dfee
Kirill S. <93134634+CasualMe@users.noreply.github.com>
2025-03-14 21:56:53 +0500

    Update Test.cs

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -59,46 +59,14 @@ public class Test
     }
 
     [Fact]
-    public void TestRectangleLarge()
+    public void TestBinomialCoefficientMiddle()
     {
-       float [,] testMatrix = new float[12,6] 
+       bool[] variants = new bool[] {true,false};
+
+       foreach(bool variant in variants)
        {
-           {8, 9, 2, 1, 6, 9},
-           {2, 3, 4, 8, 5, 1},
-           {4, 1, 7, 7, 1, 7},
-           {5, 6, 2, 8, 5, 6},
-           {3, 5, 2, 5, 8, 3},
-           {6, 9, 1, 3, 1, 5},
-           {7, 5, 4, 4, 2, 9},
-           {8, 7, 4, 1, 3, 5},
-           {6, 5, 7, 7, 6, 2},
-           {6, 2, 4, 8, 6, 3},
-           {7, 7, 2, 4, 5, 7},
-           {3, 8, 1, 6, 7, 1},
-       };
+         Assert.Equal(252, BinomialCoefficient(10,5,variant));
+       }
         
-       List<int[]> path = new List<int[]>() 
-       {
-                    {new int[2]{0,0}},
-                    {new int[2]{1,0}},
-                    {new int[2]{1,1}},
-                    {new int[2]{2,1}},
-                    {new int[2]{3,1}},
-                    {new int[2]{3,2}},
-                    {new int[2]{4,2}},
-                    {new int[2]{5,2}},
-                    {new int[2]{5,3}},
-                    {new int[2]{5,4}},
-                    {new int[2]{6,4}},
-                    {new int[2]{7,4}},
-                    {new int[2]{7,5}},
-                    {new int[2]{8,5}},
-                    {new int[2]{9,5}},
-                    {new int[2]{10,5}},
-                    {new int[2]{11,5}},
-       };
-       Task.LengthPathObject result = new Task.LengthPathObject(52, path);
-        
-       Assert.Equivalent(result, Program.CalculatePath(testMatrix));
     }
 }
