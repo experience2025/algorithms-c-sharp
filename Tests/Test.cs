20048aeb81047364980e96d8fc981329eeeb8ffd
Kirill S. <93134634+CasualMe@users.noreply.github.com>
2025-03-14 22:15:47 +0500

    Update Test.cs

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -51,7 +51,7 @@ public class Test
             {
                 foreach(bool variant in variants)
                 {
-                    int res = Program.BinomialCoefficient(i,j+1,variant);
+                    int res = Program.BinomialCoefficient(i,j,variant);
                     Assert.Equal(res, pascals_triangle[i][j]);
                 }
             }
