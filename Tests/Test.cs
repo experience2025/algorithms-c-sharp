2b708ea099411ec156fc8eb4ce2a82a08c72727c
Kirill S. <93134634+CasualMe@users.noreply.github.com>
2025-03-15 21:20:30 +0500

    Update Test.cs

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -61,13 +61,40 @@ public class Test
     public void TestTripleChar()
     {
         Assert.Equivalent(new string[] {"a","b","c"}, Program.GeneratePermutations(new string[] {"a","b","c"})[0]);
-        Assert.Equivalent(new string[] {"a","c","b"}, Program.GeneratePermutations(new string[] {"a","b","c"})[0]);
-        Assert.Equivalent(new string[] {"b","a","c"}, Program.GeneratePermutations(new string[] {"a","b","c"})[0]);
-        Assert.Equivalent(new string[] {"b","c","a"}, Program.GeneratePermutations(new string[] {"a","b","c"})[0]);
-        Assert.Equivalent(new string[] {"c","a","b"}, Program.GeneratePermutations(new string[] {"a","b","c"})[0]);
-        Assert.Equivalent(new string[] {"c","b","a"}, Program.GeneratePermutations(new string[] {"a","b","c"})[0]);
+        Assert.Equivalent(new string[] {"a","c","b"}, Program.GeneratePermutations(new string[] {"a","b","c"})[1]);
+        Assert.Equivalent(new string[] {"b","a","c"}, Program.GeneratePermutations(new string[] {"a","b","c"})[2]);
+        Assert.Equivalent(new string[] {"b","c","a"}, Program.GeneratePermutations(new string[] {"a","b","c"})[3]);
+        Assert.Equivalent(new string[] {"c","a","b"}, Program.GeneratePermutations(new string[] {"a","b","c"})[4]);
+        Assert.Equivalent(new string[] {"c","b","a"}, Program.GeneratePermutations(new string[] {"a","b","c"})[5]);
     }
 
-
+    [Fact]
+    public void TestQuadrupleNum()
+    {
+        Assert.Equivalent(new int[] {1,2,3,4}, Program.GeneratePermutations(new int[] {1,2,3,4})[0]);
+        Assert.Equivalent(new int[] {1,2,4,3}, Program.GeneratePermutations(new int[] {1,2,3,4})[1]);
+        Assert.Equivalent(new int[] {1,3,2,4}, Program.GeneratePermutations(new int[] {1,2,3,4})[2]);
+        Assert.Equivalent(new int[] {1,3,4,2}, Program.GeneratePermutations(new int[] {1,2,3,4})[3]);
+        Assert.Equivalent(new int[] {1,4,2,3}, Program.GeneratePermutations(new int[] {1,2,3,4})[4]);
+        Assert.Equivalent(new int[] {1,4,3,2}, Program.GeneratePermutations(new int[] {1,2,3,4})[5]);
+        Assert.Equivalent(new int[] {2,1,3,4}, Program.GeneratePermutations(new int[] {1,2,3,4})[6]);
+        Assert.Equivalent(new int[] {2,1,4,3}, Program.GeneratePermutations(new int[] {1,2,3,4})[7]);
+        Assert.Equivalent(new int[] {2,3,1,4}, Program.GeneratePermutations(new int[] {1,2,3,4})[8]);
+        Assert.Equivalent(new int[] {2,3,4,1}, Program.GeneratePermutations(new int[] {1,2,3,4})[9]);
+        Assert.Equivalent(new int[] {2,4,1,3}, Program.GeneratePermutations(new int[] {1,2,3,4})[10]);
+        Assert.Equivalent(new int[] {2,4,3,1}, Program.GeneratePermutations(new int[] {1,2,3,4})[11]);
+        Assert.Equivalent(new int[] {3,1,2,4}, Program.GeneratePermutations(new int[] {1,2,3,4})[12]);
+        Assert.Equivalent(new int[] {3,1,4,2}, Program.GeneratePermutations(new int[] {1,2,3,4})[13]);
+        Assert.Equivalent(new int[] {3,2,1,4}, Program.GeneratePermutations(new int[] {1,2,3,4})[14]);
+        Assert.Equivalent(new int[] {3,2,4,1}, Program.GeneratePermutations(new int[] {1,2,3,4})[15]);
+        Assert.Equivalent(new int[] {3,4,1,2}, Program.GeneratePermutations(new int[] {1,2,3,4})[16]);
+        Assert.Equivalent(new int[] {3,4,2,1}, Program.GeneratePermutations(new int[] {1,2,3,4})[17]);
+        Assert.Equivalent(new int[] {4,1,2,3}, Program.GeneratePermutations(new int[] {1,2,3,4})[18]);
+        Assert.Equivalent(new int[] {4,1,3,2}, Program.GeneratePermutations(new int[] {1,2,3,4})[19]);
+        Assert.Equivalent(new int[] {4,2,1,3}, Program.GeneratePermutations(new int[] {1,2,3,4})[20]);
+        Assert.Equivalent(new int[] {4,2,3,1}, Program.GeneratePermutations(new int[] {1,2,3,4})[21]);
+        Assert.Equivalent(new int[] {4,3,1,2}, Program.GeneratePermutations(new int[] {1,2,3,4})[22]);
+        Assert.Equivalent(new int[] {4,3,2,1}, Program.GeneratePermutations(new int[] {1,2,3,4})[23]);
+    }
 
 }
