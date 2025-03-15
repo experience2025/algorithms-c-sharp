b17efb0ac64154250123e69ee8db3f508adfdcba
Kirill S. <93134634+CasualMe@users.noreply.github.com>
2025-03-15 20:37:06 +0500

    Update Test.cs

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -35,15 +35,15 @@ public class Test
     [Fact]
     public void TestDoubleNum()
     {
-        Assert.Equivalent([{"1","2"}], Program.GeneratePermutations(new int[] {1,2})[0]);
-        Assert.Equivalent([{"2","1"}], Program.GeneratePermutations(new int[] {1,2})[1]);
+        Assert.Equivalent(new string[] {"1","2"}, Program.GeneratePermutations(new int[] {1,2})[0]);
+        Assert.Equivalent(new string[] {"2","1"}, Program.GeneratePermutations(new int[] {1,2})[1]);
     }
 
     [Fact]
     public void TestDoubleBool()
     {
-        Assert.Equivalent([{true,false}], Program.GeneratePermutations(new bool[] {true,false})[0]);
-        Assert.Equivalent([{false,true}], Program.GeneratePermutations(new bool[] {true,false})[1]);
+        Assert.Equivalent(new bool[] {true,false}, Program.GeneratePermutations(new bool[] {true,false})[0]);
+        Assert.Equivalent(new bool[] {false,true}, Program.GeneratePermutations(new bool[] {true,false})[1]);
     }
 
     [Fact]
