231b8d51a00074f588591016fdcb2da9552605c6
Kirill S. <93134634+CasualMe@users.noreply.github.com>
2025-03-17 04:35:49 +0500

    Update Test.cs

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -5,79 +5,18 @@ namespace Tests;
 public class Test
 {
     [Fact]
-    public void TestIncorrectLengthValues()
+    public void TestRabbits()
     {
-        Assert.Throws<InvalidOperationException>(() => Program.Generate_strings(-1));
+        Assert.Equal(1,Program.Rabbits(1,2));
+        Assert.Equal(1,Program.Rabbits(2,2));
+        Assert.Equal(2,Program.Rabbits(3,7));
+        Assert.Equal(2,Program.Rabbits(3,8));
+        Assert.Equal(3,Program.Rabbits(4,8));
+        Assert.Equal(5,Program.Rabbits(5,6));
+        Assert.Equal(8,Program.Rabbits(6,8));
+        Assert.Equal(13,Program.Rabbits(7,9));
+        Assert.Equal(20,Program.Rabbits(8,7));
+        Assert.Equal(34,Program.Rabbits(9,9));
     }
 
-    [Fact]
-    public void TestZeroOne()
-    {
-      for(int i = 1; i <= 20; i++)
-      {
-          Assert.Equal(Program.Generate_strings(i).Length, i);
-      }
-
-    }
-    
-    [Fact]
-    public void TestNLessThanK()
-    {
-        Assert.Throws<InvalidOperationException>(() => Program.BinomialCoefficient(1,5));
-    }
-
-    [Fact]
-    public void TestNegativeN()
-    {
-        Assert.Throws<InvalidOperationException>(() => Program.BinomialCoefficient(-1,5));
-    }
-
-    [Fact]
-    public void TestNegativeK()
-    {
-        Assert.Throws<InvalidOperationException>(() => Program.BinomialCoefficient(5,-1));
-    }
-
-    [Fact]
-    public void TestBinomialCoefficientTiny()
-    {
-        int [][] pascals_triangle = new int [][] {new int[]{1}, new int[]{1, 1}, new int[]{1, 2, 1}, new int[]{1, 3, 3, 1}, new int[]{1, 4, 6, 4, 1}};
-        bool[] variants = new bool[] {true,false};
-
-        for(int i = 0; i < pascals_triangle.Length; i++)
-        {
-            for(int j = 0; j < pascals_triangle[i].Length; j++)
-            {
-                foreach(bool variant in variants)
-                {
-                    int res = Program.BinomialCoefficient(i,j,variant);
-                    Assert.Equal(res, pascals_triangle[i][j]);
-                }
-            }
-        }
-    }
-
-    [Fact]
-    public void TestBinomialCoefficientMiddle()
-    {
-       bool[] variants = new bool[] {true,false};
-
-       foreach(bool variant in variants)
-       {
-         Assert.Equal(252, Program.BinomialCoefficient(10,5,variant));
-       }
-        
-    }
-
-     [Fact]
-    public void TestBinomialCoefficientLarge()
-    {
-       bool[] variants = new bool[] {true,false};
-
-       foreach(bool variant in variants)
-       {
-         Assert.Equal(30045015, Program.BinomialCoefficient(30,20,variant));
-       }
-        
-    }
 }
